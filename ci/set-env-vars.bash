23c5b2af1db7b4a724d4b58386e9eea2e64f5957
Méril <me@mpsq.org>
2021-03-04 17:22:51 +0000

    feat(ci): add shellcheck for bash code (#10)

--- ci/set-env-vars.bash
+++ ci/set-env-vars.bash
@@ -2,14 +2,16 @@
 
 set -euxo pipefail
 
+# We set PKG_VERSION to "xx.xx.xx"
 PKG_VERSION=$(
   curl -s "$UPSTREAM_SRC"/configure.ac | \
     grep AC_INIT | \
     sed -e 's/^.\+\ \([0-9]\+\.[0-9]\+\.[0-9]\+\?\).\+$/\1/'
 )
 PKG_VERSION+="."
+# We concatenate the version number with commit count: xx.xx.xx.xxxxxx
 PKG_VERSION+="$NEW_COMMIT_N"
 
+# Export to env var + github action var
 export PKG_VERSION="${PKG_VERSION}"
-
 echo "::set-output name=pkg_version::${PKG_VERSION}"
