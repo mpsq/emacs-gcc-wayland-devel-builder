3cfac3bc28f8d4d2ad526892aa5b28397c5ea137
Méril Pilon <me@mpsq.org>
2023-01-07 21:11:01 +0000

    fix: add gh token to curl api.github.com request

--- scripts/check-existing-release.bash
+++ scripts/check-existing-release.bash
@@ -2,7 +2,12 @@
 
 set -euxo pipefail
 
-status_code=$(curl -s -o "/dev/null" -w "%{http_code}" "https://api.github.com/repos/mpsq/emacs-gcc-wayland-devel-builder/releases/tags/$NEW_COMMIT_N-$PKG_REL")
+status_code=$(curl \
+  -s \
+  -o "/dev/null" \
+  -w "%{http_code}" \
+  --header "authorization: Bearer $TKN" \
+  "https://api.github.com/repos/mpsq/emacs-gcc-wayland-devel-builder/releases/tags/$PKG_VERSION-$PKG_REL")
 
 if [[ "$status_code" == "200" ]]; then
   exit 1
